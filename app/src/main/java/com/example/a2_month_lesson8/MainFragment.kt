fe8e32dc333557f5054e79604cd4fe5bbff0793c
UrazbaevT <utilek990@gmail.com>
2023-01-04 20:24:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a2_month_lesson8/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.example.a2_month_lesson8
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.a2_month_lesson8.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var navArgs: MainFragmentArgs
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            navArgs = MainFragmentArgs.fromBundle(it)
+        }
+
+        binding.tvLogin.text = navArgs.user.login
+    }
+
+
+
+}
